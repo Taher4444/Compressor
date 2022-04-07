ed4c7bdd1350e005f976351a8a999071d5101c11
Vivek Tomar <86093210+im-vivektomar@users.noreply.github.com>
2022-04-07 13:44:28 +0530

    Add files via upload

---
+++ LOCAL/localisation.py
@@ -0,0 +1,42 @@
+#Don't be a thief by stealing other's Hardwork, it took time and effort to make this repo.
+#Respect my work by not making any changes here.
+
+START_TEXT = "𝐇ᴇʟʟᴏ 👋,\n\n𝐓𝐡𝐢𝐬 𝐢𝐬 𝐚 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐕𝐢𝐝𝐞𝐨 𝐂𝐨𝐦𝐩𝐫𝐞𝐬𝐬 𝐁𝐨𝐭\n\n𝐏𝐥𝐞𝐚𝐬𝐞 𝐬𝐞𝐧𝐭 𝐦𝐞 𝐚𝐧𝐲 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐛𝐢𝐠 𝐟𝐢𝐥𝐞 𝐈 𝐖𝐢𝐥𝐥 𝐜𝐨𝐦𝐩𝐫𝐞𝐬𝐬 𝐚 𝐬𝐦𝐚𝐥𝐥 𝐟𝐢𝐥𝐞\n\n𝐂ʟɪᴄᴋ 𝐎ɴ /help 𝐅ᴏʀ 𝐌ᴏʀᴇ 𝐃ᴇᴛᴀɪʟ𝐬\n\n𝐏ᴏᴡᴇʀᴅ 𝐁ʏ : @ChVivekTomar"
+
+FORCE_SUB_TEXT = "In order to use this bot, you've to join my parent channel."
+
+CHANNEL_LINK = "https://t.me/Movies_Demand_Hub"
+
+SUPPORT_LINK = "https://t.me/Movies_Demand_Hub"
+
+info_text = "This bot is developed by @ChVivekTomar\n\nWritten in python library TELETHON.\n\nBot by : Vivek\nSupport : @ChVivekTomar\n\nV1.4"   
+
+help_text = """**v1.4**
+
+•`Encode` - encode your video into different lib format or resolution
+
+•`HEVC compress` - negligible loss compression
+
+•`FAST compress` - Very fast and Efficient compression
+
+•`Convert` - change formats or extract audio of any video
+
+•`Rename` - rename any file, extension not required
+
+•`SSHOTS` - generate 10 screenshots of your video
+
+•`Trim` - cut your videos"""
+
+source_text = "**Deploy your own bot**"
+
+DEV = "https://t.me/ChVivekTomar"
+
+spam_notice = "This bot is hosted on heroku, and hence can just run one process at a time.Spamming the bot or encoding adult videos will lead you to a ban."
+
+JPG = "LOCAL/video_convertor.jpg"
+
+JPG0 = 'https://telegra.ph/file/d98c559b56ef884ef3bad.jpg'
+
+JPG2 = "LOCAL/20211215_165751.jpg"
+
+JPG3 = "LOCAL/PicsArt_12-16-08.57.15.jpg"
